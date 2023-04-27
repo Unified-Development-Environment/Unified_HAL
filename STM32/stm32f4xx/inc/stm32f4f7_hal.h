0f1046db65bc5ec89cbac54fb5214a96eb89b35f
rish292 <104636130+rish292@users.noreply.github.com>
2023-04-27 16:10:03 +0530

    Add files via upload

---
+++ STM32/stm32f4xx/inc/stm32f4f7_hal.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f4f7_hal.h
+ *
+ *  Created on: Apr 27, 2023
+ *      Author: apple
+ */
+
+#ifndef INC_STM32F4F7_HAL_H_
+#define INC_STM32F4F7_HAL_H_
+
+void GPIO_Write(char* PORT, int PIN, int VALUE);
+
+#endif /* INC_STM32F4F7_HAL_H_ */
