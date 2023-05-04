8cdae47491e079a3f1428804b7e7cec2caba0342
rish292 <104636130+rish292@users.noreply.github.com>
2023-05-04 21:46:49 +0530

    Fixed texas source file dependency

--- Texas_Instruments/Tiva_TM4C/inc/texastm4c_hal.h
+++ Texas_Instruments/Tiva_TM4C/inc/texastm4c_hal.h
@@ -1,3 +1,9 @@
+#ifndef uf_gpio_h
+#define uf_gpio_h
+
+#include "TExaS.h"
+
 #define GPIO_PORTF_DATA_R   (*((volatile unsigned long *) 0x400253FC))
 
-void uf_gpioWrite(char* PORT, int PIN, int VALUE);
+void uf_gpioWrite(char* PORT, int PIN, int VALUE);  
+#endif
--- Texas_Instruments/Tiva_TM4C/src/texastm4c.c
+++ Texas_Instruments/Tiva_TM4C/src/texastm4c.c
@@ -2,10 +2,10 @@
 
 void uf_gpioWrite(char* PORT, int PIN, int VALUE) {
 
-    if(VALUE == 0) {
-        GPIO_PORTF_DATA_R = 0x00;
+    if(VALUE == 0) {    
+        GPIO_PORTF_DATA_R = 0x00;   //LED is off
     } else {
-        GPIO_PORTF_DATA_R = 0x04;
+        GPIO_PORTF_DATA_R = 0x04;   //LED is on
     }
 
 }
