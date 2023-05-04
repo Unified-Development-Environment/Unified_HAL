1b242384e7fb69fa452dafe0de4040e2fdd72adb
rish292 <104636130+rish292@users.noreply.github.com>
2023-05-04 11:00:43 +0530

    Final function name updated

--- Texas_Instruments/Tiva_TM4C/inc/texastm4c_hal.h
+++ Texas_Instruments/Tiva_TM4C/inc/texastm4c_hal.h
@@ -1,3 +1,3 @@
 #define GPIO_PORTF_DATA_R   (*((volatile unsigned long *) 0x400253FC))
 
-void GPIO_Write(char* PORT, int PIN, int VALUE);
+void uf_gpioWrite(char* PORT, int PIN, int VALUE);
--- Texas_Instruments/Tiva_TM4C/src/texastm4c.c
+++ Texas_Instruments/Tiva_TM4C/src/texastm4c.c
@@ -1,6 +1,6 @@
 #include "texastm4x_hal.h"
 
-void GPIO_Write(char* PORT, int PIN, int VALUE) {
+void uf_gpioWrite(char* PORT, int PIN, int VALUE) {
 
     if(VALUE == 0) {
         GPIO_PORTF_DATA_R = 0x00;
