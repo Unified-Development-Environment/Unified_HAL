97c8cf8c7c95367e342323d1a2829b3e4355e67d
rish292 <104636130+rish292@users.noreply.github.com>
2023-05-11 13:59:16 +0530

    Added uf_gpioRead

--- Texas_Instruments/Tiva_TM4C/inc/texastm4c_hal.h
+++ Texas_Instruments/Tiva_TM4C/inc/texastm4c_hal.h
@@ -6,4 +6,6 @@
 #define GPIO_PORTF_DATA_R   (*((volatile unsigned long *) 0x400253FC))
 
 void uf_gpioWrite(char* PORT, int PIN, int VALUE);  
+int uf_gpioRead(char* PORT, int PIN);
+
 #endif
--- Texas_Instruments/Tiva_TM4C/src/texastm4c.c
+++ Texas_Instruments/Tiva_TM4C/src/texastm4c.c
@@ -1,11 +1,30 @@
 #include "texastm4x_hal.h"
 
+unsigned uint_pow(unsigned base, unsigned exp)
+{
+    unsigned result = 1;
+    while (exp)
+    {
+        if (exp % 2)
+           result *= base;
+        exp /= 2;
+        base *= base;
+    }
+    return result;
+}
+
 void uf_gpioWrite(char* PORT, int PIN, int VALUE) {
 
     if(VALUE == 0) {    
-        GPIO_PORTF_DATA_R = 0x00;   //LED is off
+        GPIO_PORTF_DATA_R = 0;   //LED is off
     } else {
-        GPIO_PORTF_DATA_R = 0x04;   //LED is on
+        GPIO_PORTF_DATA_R = uint_pow(2, PIN);   //LED is on
     }
+
+}
+
+int uf_gpioRead(char* PORT, int PIN) {
+
+	return GPIO_PORTF_DATA_R & uint_pow(2, PIN);
 
 }
